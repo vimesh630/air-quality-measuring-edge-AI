5f4ac198752c96451cdd59f87f9875714c95afc8
sakuna sankalpa <sakunasankalpa@gmail.com>
2026-04-18 10:55:20 +0530

    import library

---
+++ sensor_read.py
@@ -0,0 +1,43 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyOQn+uJVP7Dcu+ZxE7cG7mj",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/vimesh630/air-quality-measuring-edge-AI/blob/feature%2Fhardware-sensors/sensor_read.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "ojkTy9fJ_qEz"
+      },
+      "outputs": [],
+      "source": [
+        "import random\n",
+        "import math\n",
+        "import time\n"
+      ]
+    }
+  ]
+}
